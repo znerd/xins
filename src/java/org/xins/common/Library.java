ab3e86970aed44b6795ca4f8d2d58c5acef6c2a4
Ernst de Haan <ernst@pensioenpage.com>
2010-02-06 20:55:06 +0100

    Reading version information from META-INF/version.txt.

--- src/java/org/xins/common/Library.java
+++ src/java/org/xins/common/Library.java
@@ -6,6 +6,12 @@
  */
 package org.xins.common;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+
 /**
  * Class that represents the XINS/Java Common Library.
  *
@@ -17,10 +23,41 @@ package org.xins.common;
 public final class Library {
 
    /**
+    * The version of this library, lazily initialized.
+    */
+   private static String VERSION;
+
+   /**
+    * Initializes this class, loading the version number once.
+    */
+   static {
+      String filePath = "version.txt";
+      try {
+         InputStream stream = getMetaResourceAsStream(Library.class, filePath);
+         VERSION = IOUtils.toString(stream, "UTF-8").trim();
+      } catch (IOException cause) {
+         System.err.println("I/O error while reading meta resource: " + filePath);
+      }
+   }
+
+   /**
     * Constructs a new <code>Library</code> object.
     */
    private Library() {
       // empty
+   }
+   
+   /**
+    * Returns the name of this library.
+    * 
+    * @return
+    *    the name of this library, never <code>null</code>;
+    *    for example <code>"XINS/Java Common Library"</code>.
+    *    
+    * @since XINS 3.0
+    */
+   public static final String getName() {
+      return "XINS/Java Common Library";
    }
 
    /**
@@ -31,6 +68,87 @@ public final class Library {
     *    never <code>null</code>.
     */
    public static final String getVersion() {
-      return Library.class.getPackage().getImplementationVersion();
+      return VERSION;
+   }
+   
+   /**
+    * Prints the name and version of this library.
+    * 
+    * @param args
+    *    the command line arguments; will be ignored.
+    *
+    * @since XINS 3.0
+    */
+   public static final void main(String[] args) {
+      System.out.println(getName() + " " + getVersion());
+   }
+   
+   /**
+    * Retrieves a meta resource and returns it as a <code>URL</code>.
+    * 
+    * @param clazz
+    *    the class that relates to the resource, cannot be <code>null</code>.
+    *    
+    * @param path
+    *    the path to the meta resource, cannot be <code>null</code>.
+    *    
+    * @return
+    *    the resource as a {@link URL}, never <code>null</code>.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>path == null</code>.
+    *    
+    * @throws NoSuchResourceException
+    *    if the resource could not be found.
+    *
+    * @since XINS 3.0
+    */
+   public static final <T> URL getMetaResource(Class<T> clazz, String path)
+   throws IllegalArgumentException, NoSuchResourceException {
+      
+      // Check preconditions
+      MandatoryArgumentChecker.check("clazz", clazz, "path", path);
+      
+      // Load the resource
+      String absPath = "/META-INF/" + path;
+      URL        url = clazz.getResource(absPath);
+      
+      // Resource not found - this is fatal
+      if (url == null) {
+         // TODO: Log.log_XXX(LogLevel.ERROR, "Failed to load resource \"" + absPath + "\".");
+         throw new NoSuchResourceException("Failed to load resource \"" + absPath + "\".");
+      }
+      
+      // TODO: Log.log_XXX(LogLevel.DEBUG, "Loaded \"" + absPath + "\".");
+      
+      return url;
+   }
+   
+   /**
+    * Retrieves a meta resource and returns it as an <code>InputStream</code>.
+    * 
+    * @param clazz
+    *    the class that relates to the resource, cannot be <code>null</code>.
+    *    
+    * @param path
+    *    the path to the meta resource, cannot be <code>null</code>.
+    *    
+    * @return
+    *    the resource as an {@link InputStream}.
+    *
+    * @throws IllegalArgumentException
+    *    if <code>path == null</code>.
+    *    
+    * @throws NoSuchResourceException
+    *    if the resource could not be found.
+    *    
+    * @throws IOException
+    *    if the stream could not be opened.
+    *
+    * @since XINS 3.0
+    */
+   public static final <T> InputStream getMetaResourceAsStream(Class<T> clazz, String path)
+   throws IllegalArgumentException, NoSuchResourceException, IOException {
+      return getMetaResource(clazz, path).openStream();
    }
 }
